fa03ce66539aa98f790c7671461cf89208fa9d98
Mingfei Huang <max0ne@users.noreply.github.com>
2018-09-28 19:07:44 -0400

    [auto created commit by LeetGlue] permutation-in-string.py

---
+++ permutation-in-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        c1 = collections.Counter(s1)
+        c2 = collections.Counter()
+        lo = 0
+        for hi in range(len(s2)):
+            c2[s2[hi]] += 1
+            if all(c1[ch] == c2[ch] for ch in c1.keys()):
+                return True
+            while any(c1[key] < c2[key] for key in c2.keys()):
+                c2[s2[lo]] -= 1
+                lo += 1
+        return False
