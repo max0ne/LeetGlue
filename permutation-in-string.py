6d8c3f740bfeb7a90e7c0d5e8bd5aed8e30db48f
Mingfei Huang <max0ne@users.noreply.github.com>
2018-09-28 19:14:50 -0400

    [auto created commit by LeetGlue] permutation-in-string.py

--- permutation-in-string.py
+++ permutation-in-string.py
@@ -5,14 +5,16 @@ class Solution:
         :type s2: str
         :rtype: bool
         """
-        c1 = collections.Counter(s1)
-        c2 = collections.Counter()
+        c1 = [0] * 26
+        c2 = [0] * 26
+        for ch in s1:
+            c1[ord(ch) - ord('a')] += 1
         lo = 0
         for hi in range(len(s2)):
-            c2[s2[hi]] += 1
-            if all(c1[ch] == c2[ch] for ch in 'werqtyuiopasdfghjklzxcvbnm'):
+            c2[ord(s2[hi]) - ord('a')] += 1
+            if c1 == c2:
                 return True
-            while any(c1[key] < c2[key] for key in 'werqtyuiopasdfghjklzxcvbnm'):
-                c2[s2[lo]] -= 1
+            while any(c2[key] > c1[key] for key in range(26)):
+                c2[ord(s2[lo]) - ord('a')] -= 1
                 lo += 1
         return False
